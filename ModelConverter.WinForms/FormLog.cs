bdb69f9bdc9f0a03e1974fd4fdf472b6ef608528
Sven Tatter <sven31894@gmail.com>
2013-02-14 22:04:47 +0100

    split multiline Log Messages into single lines

--- ModelConverter.WinForms/FormLog.cs
+++ ModelConverter.WinForms/FormLog.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using ModelConverter.Model;
+using System.IO;
 
 namespace ModelConverter.WinForms
 {
@@ -20,23 +21,32 @@ namespace ModelConverter.WinForms
 
 		public void Log(LogLevel lvl, string Message)
 		{
-			ListViewItem item = new ListViewItem(new string[] { DateTime.Now.ToString(), lvl.ToString(), Message });
-			switch (lvl)
+			// loop over all lines in message
+			using (StringReader reader = new StringReader(Message))
 			{
-				case LogLevel.Fatal:
-					item.BackColor = Color.DarkRed;
-					item.ForeColor = Color.White;
-					break;
-				case LogLevel.Error:
-					item.BackColor = Color.Red;
-					item.ForeColor = Color.White;
-					break;
-				case LogLevel.Warning:
-					item.BackColor = Color.Orange;
-					item.ForeColor = Color.White;
-					break;
+				string line;
+				while ((line = reader.ReadLine()) != null)
+				{
+					ListViewItem item = new ListViewItem(new string[] { DateTime.Now.ToString(), lvl.ToString(), line });
+					switch (lvl)
+					{
+						case LogLevel.Fatal:
+							item.BackColor = Color.DarkRed;
+							item.ForeColor = Color.White;
+							break;
+						case LogLevel.Error:
+							item.BackColor = Color.Red;
+							item.ForeColor = Color.White;
+							break;
+						case LogLevel.Warning:
+							item.BackColor = Color.Orange;
+							item.ForeColor = Color.White;
+							break;
+					}
+					listView1.Items.Add(item);
+				}
 			}
-			listView1.Items.Add(item);
+
 			listView1.EnsureVisible(listView1.Items.Count - 1);
 
 			listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
